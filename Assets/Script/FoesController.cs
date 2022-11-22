8a9c6cd3645e4ff5c61b5191d676b0476a9541ab
Charlie Boyer <charlieboyer@outlook.fr>
2022-11-22 17:02:24 +0100

    Update .gitignore and fix missprint in FoesController

--- Assets/Script/FoesController.cs
+++ Assets/Script/FoesController.cs
@@ -11,7 +11,7 @@ namespace Script
         void Awake()
         {
             prototypeColor = Color.red;
-            _prototypeMeshRenderer
+            // _prototypeMeshRenderer
         }
 
         void Update()
