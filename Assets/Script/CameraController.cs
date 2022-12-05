e584d348bedab2cac67b0cc1377b07ecdae0236f
Charlie BOYER <charlieboyer@outlook.fr>
2022-12-05 01:09:15 +0100

    Setup Cinemachine scripting assets and slightly refactor
    * Refactor cursorLock properties in a dedicated method
    + Gather Cinemachine brain & cameras
    + Add a public method for rotating in-sync with player position (basic camera system)

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,23 +1,45 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using UnityEditor;
 
 namespace Script
 {
     public class CameraController : MonoBehaviour
     {
+        public bool cursorLock = true;
         public float sensitivityX = 1f;
         public float sensitivityY = 1f;
-        private const float InnateSensitivity = 100f;
 
-        private void Awake()
+        [SerializeField] private float InnateSensitivity = 100f;
+
+        private CinemachineBrain _cameraBrain;
+        public CinemachineVirtualCamera _activeCamera;
+        static CinemachineVirtualCamera[] _vCameraList;
+
+        private void Start()
         {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
+            LockCursor();
+            SetupCameras();
         }
 
-        
+        private void SetupCameras()
+        {
+            _cameraBrain = CinemachineCore.Instance.GetActiveBrain(0);
+            _activeCamera = _cameraBrain.ActiveVirtualCamera as CinemachineVirtualCamera;
+            _vCameraList = GameObject.FindObjectsOfType<CinemachineVirtualCamera>();
+        }
 
+        private void LockCursor()
+        {
+            if (cursorLock == true) {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+
+        public void Rotate(Quaternion targetRotation)
+        {
+            _activeCamera.transform.rotation = targetRotation;
+        }
     }
 }
